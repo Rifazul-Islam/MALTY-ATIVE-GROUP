c12cbbc4488564faf77525a242873bcc7a1f0718
Rifazul <rifazul50@gmail.com>
2022-09-29 02:08:13 +0600

    add destop resposiv

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -20,24 +20,30 @@ const Exercises = () => {
 
 
     return (
-        <div className="exercises-container">
 
-            <div className="left-site">
-                <h2 style={{ color: 'green' }}> MOLTY-Ative-Group</h2>
-                <h3>Seleced Everyday Exercise</h3>
-                {
-                    excercises.map(excercise =>
-                        <Exercise key={excercise.name}
-                            excercise={excercise}
-                        ></Exercise>)
-                }
+        <div>
+            <h2 style={{ color: 'green', marginLeft: '20px' }
+            }> MOLTY - Ative - Group</h2 >
+            <h3 style={{ marginLeft: '20px' }
+            }>Seleced Everyday Exercise</h3>
+            <div className="exercises-container">
+
+                <div className="left-site">
+
+                    {
+                        excercises.map(excercise =>
+                            <Exercise key={excercise.name}
+                                excercise={excercise}
+                            ></Exercise>)
+                    }
+                </div>
+                <div className="right-site">
+
+                    <h2>Oder Summary</h2>
+
+                </div>
+
             </div>
-            <div className="right-site">
-
-                <h2>Oder Summary</h2>
-
-            </div>
-
         </div>
     );
 };
